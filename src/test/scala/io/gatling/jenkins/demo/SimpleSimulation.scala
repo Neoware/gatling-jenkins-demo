bf753026d2287e0c4d3e11af872e771e1e79c579
Neoware <alexandre.chaouat@gmail.com>
2017-06-12 15:45:31 +0200

    wip

--- src/test/scala/io/gatling/jenkins/demo/SimpleSimulation.scala
+++ src/test/scala/io/gatling/jenkins/demo/SimpleSimulation.scala
@@ -2,6 +2,7 @@ package io.gatling.jenkins.demo
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import scala.concurrent.duration._
 
 class SimpleSimulation extends Simulation {
 
